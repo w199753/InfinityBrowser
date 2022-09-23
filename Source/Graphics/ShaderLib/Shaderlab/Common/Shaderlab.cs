55efb86548f931d2e9f6d4d544f2948568d1dea2
haolange <1824339952@qq.com>
2022-09-23 18:28:12 +0800

    Update

--- Source/Graphics/ShaderLib/Shaderlab/Common/Shaderlab.cs
+++ Source/Graphics/ShaderLib/Shaderlab/Common/Shaderlab.cs
@@ -1,10 +1,5 @@
 ﻿using Infinity.Mathmatics;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infinity.Shaderlib
 {
--- Source/Graphics/ShaderLib/Shaderlab/ShaderLabGenerator.cs
+++ Source/Graphics/ShaderLib/Shaderlab/ShaderLabGenerator.cs
@@ -1,13 +1,22 @@
-﻿using Infinity.Container;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
 using System.Text;
+using Infinity.Container;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using Infinity.Mathmatics;
 
 namespace Infinity.Shaderlib
 {
-    public class ShaderLabGenerator : ShaderLabBaseVisitor<TAny>
+    public struct ShaderLabCompileResult
+    {
+        string Name;
+        int IntParam;
+        float FloatParam;
+        float4 VectorParam;
+    };
+
+    public class ShaderLabGenerator : ShaderLabBaseVisitor<ShaderLabCompileResult>
     {
 
     }
