d22ec3fad9284aa1e00e3100a52b95483e8b674c
‘haolange’ <1824339952@qq.com>
2022-11-21 02:27:16 +0800

    Update

--- Source/Engine/Application.cs
+++ Source/Engine/Application.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using Infinity.Threading;
-using Infinity.Windowing;
-using System.Runtime.InteropServices;
-using System.Runtime.CompilerServices;
 using Silk.NET.Input;
 using Silk.NET.Maths;
 using System.Numerics;
+using System.Threading;
+using Infinity.Windowing;
 using Infinity.Rendering;
+using Infinity.Analytics;
+using System.Collections.Generic;
 
 namespace Infinity.Engine
 {
@@ -28,49 +28,24 @@ namespace Infinity.Engine
                 return m_GameWorld;
             }
         }
-
-        // Window
+        
         private PlatformWindow m_Window;
-
-        // World
         private GameWorld m_GameWorld;
-
-        // Thread
-        private Semaphore m_SemaphoreG2R;
-        private Semaphore m_SemaphoreR2G;
-
-        // Module
-        private GameModule m_GameModule;
-        //private PhysicsModule m_PhysicsModule;
+        private float m_DeltaTime;
+        private TimeProfiler m_TimeCounter;
+        private List<float> m_LastDeltaTimes;
+        private PhysicsModule m_PhysicsModule;
         private GraphicsModule m_GraphicsModule;
 
         public Application(in int width, in int height, string name)
         {
+            Thread.CurrentThread.Name = "GameThread";
             m_Window = new PlatformWindow(width, height, name, OnResize, OnFocus, OnMouseUp, OnMouseDown, OnMouseMove, OnMouseClick, OnMouseDoubleClick, OnMouseScroll, OnKeyUp, OnKeyDown, OnKeyChar);
-
             m_GameWorld = new GameWorld();
-
-            m_SemaphoreR2G = new Semaphore(true);
-            m_SemaphoreG2R = new Semaphore(false);
-
-            m_GameModule = new GameModule(End, Play, Tick, m_SemaphoreG2R, m_SemaphoreR2G);
-            //m_PhysicsModule = new PhysicsModule();
-            m_GraphicsModule = new GraphicsModule(m_Window, m_SemaphoreG2R, m_SemaphoreR2G);
-        }
-
-        protected void Play()
-        {
-            m_GameWorld.OnEnable();
-        }
-
-        protected void Tick()
-        {
-            m_GameWorld.OnUpdate(Timer.DeltaTime);
-        }
-
-        protected void End()
-        {
-            m_GameWorld.OnDisable();
+            m_TimeCounter = new TimeProfiler();
+            m_LastDeltaTimes = new List<float>(64);
+            m_PhysicsModule = new PhysicsModule();
+            m_GraphicsModule = new GraphicsModule(m_Window);
         }
 
         protected void OnFocus(bool state)
@@ -149,29 +124,83 @@ namespace Infinity.Engine
 
         public void Run()
         {
-            m_GameModule.Start();
-            //m_PhysicsModule.Start();
+            m_TimeCounter.Reset();
+            m_TimeCounter.Begin();
+
+            m_GameWorld.Start();
+            m_PhysicsModule.Start();
             m_GraphicsModule.Start();
 
             while (IsRunning)
             {
-                m_Window.Update();
-                m_GameModule.Update();
+                m_TimeCounter.Start();
+                Timer.Tick(m_DeltaTime);
+
+                m_Window.Update(m_DeltaTime);
+                m_GameWorld.Update(m_DeltaTime);
+                m_PhysicsModule.Update(m_DeltaTime);
+                m_GraphicsModule.Update(m_DeltaTime);
+                WaitForTargetFPS();
             }
 
-            m_GameModule.Exit();
-            //m_PhysicsModule.Exit();
+            m_TimeCounter.Stop();
+            m_GameWorld.Exit();
+            m_PhysicsModule.Exit();
             m_GraphicsModule.Exit();
         }
 
         protected override void Release()
         {
             m_Window.Dispose();
-            m_SemaphoreR2G.Dispose();
-            m_SemaphoreG2R.Dispose();
-            m_GameModule.Dispose();
-            //m_PhysicsModule.Dispose();
+            m_PhysicsModule.Dispose();
             m_GraphicsModule.Dispose();
+        }
+
+        void WaitForTargetFPS()
+        {
+            long elapsed;
+            const int deltaTimeSmoothing = 2;
+
+            if (GTargetFrameRate > 0)
+            {
+                long targetMax = 1000000L / GTargetFrameRate;
+
+                while (true)
+                {
+                    elapsed = m_TimeCounter.microseconds;
+                    if (elapsed >= targetMax)
+                        break;
+
+                    // Sleep if 1 ms or more off the frame limiting goal
+                    if (targetMax - elapsed >= 1000L)
+                    {
+                        int sleepTime = (int)((targetMax - elapsed) / 1000L);
+                        Thread.Sleep(sleepTime);
+                    }
+                }
+            }
+
+            elapsed = m_TimeCounter.microseconds;
+            m_TimeCounter.Start();
+
+            // Perform timestep smoothing
+            m_DeltaTime = 0.0f;
+            m_LastDeltaTimes.Add(elapsed / 1000000.0f);
+
+            if (m_LastDeltaTimes.Count > deltaTimeSmoothing)
+            {
+                // If the smoothing configuration was changed, ensure correct amount of samples
+                m_LastDeltaTimes.RemoveRange(0, m_LastDeltaTimes.Count - deltaTimeSmoothing);
+                for (int i = 0; i < m_LastDeltaTimes.Count; ++i)
+                {
+                    m_DeltaTime += m_LastDeltaTimes[i];
+                }
+                m_DeltaTime /= m_LastDeltaTimes.Count;
+            }
+            else
+            {
+                m_DeltaTime = m_LastDeltaTimes[m_LastDeltaTimes.Count - 1];
+            }
         }
     }
 }
